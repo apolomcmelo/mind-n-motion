82105954106688a30c93f38200336b2fe8e2d274
apolomcmelo <apolomcmelo@users.noreply.github.com>
2024-11-24 13:34:32 +0000

    deploy: 305b9e803d7fecab4025200ea9f0ee5fb4a039e0

--- scripts.js
+++ scripts.js
@@ -7,6 +7,41 @@ const metrics = [
     { name: "Stress", score: 0 }
 ]
 
+const appProperties = {
+    dom: {
+        startStopButton: document.getElementById('start-stop-button'),
+        startStopButtonIcon: document.getElementById('start-stop-button-icon'),
+        speedometer: document.getElementById("speedometer")
+    },
+    watchId: null,
+    speedHistory: []
+}
+
+appProperties.dom.startStopButton.addEventListener('click', (event) => {
+    if (appProperties.watchId) {
+        navigator.geolocation.clearWatch(appProperties.watchId);
+
+        appProperties.watchId = null;
+        console.log("Speed History", appProperties.speedHistory)
+
+    } else {
+        appProperties.watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                updateMap(position.coords)
+                updateSpeed(position)
+            },
+            (error) => console.error("Error watching the position", error),
+            {enableHighAccuracy: true});
+
+    }
+    toggleStartStop();
+});
+
+function toggleStartStop() {
+    appProperties.dom.startStopButtonIcon.classList.toggle("ri-play-fill");
+    appProperties.dom.startStopButtonIcon.classList.toggle("ri-stop-fill");
+}
+
 function updateMetricsScore() {
     const radius = 28; // Radius of the ring bars
     const circumference = 2 * Math.PI * radius;
@@ -16,23 +51,31 @@ function updateMetricsScore() {
         const progressCircle = metricElement.getElementsByClassName("progress-ring")[0]
         const scoreLabel = metricElement.getElementsByClassName("percentage-label")[0]
 
-        progressCircle.setAttribute('stroke-dasharray', circumference + "");
-        progressCircle.setAttribute('stroke-dashoffset', (circumference - (metric.score / 100) * circumference) + "");
+        progressCircle.setAttribute('stroke-dasharray', `${circumference}`);
+        progressCircle.setAttribute('stroke-dashoffset', `${circumference - (metric.score / 100) * circumference}`);
         scoreLabel.textContent = `${metric.score}`;
     })
 }
 
-function loadMap(location) {
+function updateMap(position) {
     const map = document.getElementById("map")
-    const mapsUri = `https://www.google.com/maps/embed/v1/place?key=${config.GOOGLE_MAPS_API_KEY}&q=${location.latitude},${location.longitude}`
+    let currentPosition = `${position.latitude},${position.longitude}`;
+    // const mapsUri = `https://www.google.com/maps/embed/v1/streetview?key=${config.GOOGLE_MAPS_API_KEY}&location=${currentPosition}`
+    const mapsUri = `https://www.google.com/maps/embed/v1/place?key=${config.GOOGLE_MAPS_API_KEY}&q=${currentPosition}`
 
     map.setAttribute("src", mapsUri);
 }
 
-function getLocation() {
+function updateSpeed(position) {
+    let speedInKmPerHour = Math.round(position.coords.speed * 3.6);
+    appProperties.dom.speedometer.textContent = `${speedInKmPerHour}`
+    appProperties.speedHistory.push({timestamp: position.timestamp, speed: speedInKmPerHour})
+}
+
+function getCurrentLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            (position) => loadMap(position.coords),
+            (position) => updateMap(position.coords),
             (error) => console.error("Error getting the current location", error),
             {enableHighAccuracy: true}
         );
@@ -43,5 +86,5 @@ function getLocation() {
 
 window.onload = () => {
     updateMetricsScore()
-    getLocation()
+    getCurrentLocation()
 }
