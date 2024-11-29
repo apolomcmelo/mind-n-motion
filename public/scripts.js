6a05f46460cc5a96b93b16a06a80723634ef13fc
Apolo Melo <apolomelo@gmail.com>
2024-11-29 20:54:03 +0000

    Reorganise functions and add css for the secondary buttons when active

--- public/scripts.js
+++ public/scripts.js
@@ -9,8 +9,10 @@ const metrics = [
 
 const appProperties = {
     dom: {
+        settingsPageButton: document.getElementById("settings-page-button"),
         startStopButton: document.getElementById('start-stop-button'),
         startStopButtonIcon: document.getElementById('start-stop-button-icon'),
+        reportPageButton: document.getElementById("report-page-button"),
         speedometer: document.getElementById("speedometer")
     },
     map: null,
@@ -22,42 +24,8 @@ const appProperties = {
     journeyCoordinates: []
 }
 
-appProperties.dom.startStopButton.addEventListener('click', (event) => {
-    if (appProperties.watchId) { // It is stopping
-        navigator.geolocation.clearWatch(appProperties.watchId);
 
-        appProperties.watchId = null;
-        localStorage.setItem("SpeedHistory", JSON.stringify(appProperties.speedHistory))
-
-        metrics.forEach(metric => {
-            const singleMetricHistory = appProperties.metricsHistory.filter(m => m.name === metric.name)
-            localStorage.setItem(`${metric.name}History`, JSON.stringify(singleMetricHistory))
-        })
-
-        console.debug("Speed History", appProperties.speedHistory)
-        console.debug("Metrics History", appProperties.metricsHistory)
-
-        appProperties.speedHistory = []
-        appProperties.metricsHistory = []
-    } else { // It is starting
-        localStorage.clear()
-        appProperties.watchId = navigator.geolocation.watchPosition(
-            (position) => {
-                updatePositionOnMap(position.coords)
-                updateSpeed(position)
-            },
-            (error) => console.error("Error watching the position", error),
-            {enableHighAccuracy: true});
-
-    }
-    toggleStartStop();
-});
-
-function toggleStartStop() {
-    appProperties.dom.startStopButtonIcon.classList.toggle("ri-play-fill");
-    appProperties.dom.startStopButtonIcon.classList.toggle("ri-stop-fill");
-}
-
+// #### Metrics
 function updateMetricsScore(metric) {
     appProperties.metricsHistory.push({name: metric.name, score: metric.score, timestamp: Date.now()})
 
@@ -73,6 +41,7 @@ function updateMetricsScore(metric) {
     scoreLabel.textContent = `${metric.score}`;
 }
 
+// #### Speed
 function updateSpeed(position) {
     const speedInKmPerHour = Math.round(position.coords.speed * 3.6);
 
@@ -80,49 +49,6 @@ function updateSpeed(position) {
 
     updateChartWith({timestamp: position.timestamp, speed: speedInKmPerHour})
 }
-
-function getCurrentLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                initMap(position.coords)
-            },
-            (error) => console.error("Error getting the current location", error),
-            {enableHighAccuracy: true}
-        );
-    } else {
-        console.warn("Geolocation is not supported by this browser.");
-    }
-}
-
-function initMap(position) {
-    const latLong = [position.latitude,position.longitude];
-    appProperties.map = L.map('map').setView(latLong, 18);
-    // Add OpenStreetMap tile layer
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; Apolo Mc Melo'
-    }).addTo(appProperties.map);
-
-    L.marker(latLong).addTo(appProperties.map);
-}
-
-function updatePositionOnMap(position) {
-    const latLong = [position.latitude,position.longitude];
-
-    appProperties.journeyCoordinates.push(latLong);
-    appProperties.map.setView(latLong, 18);
-
-    L.polyline(appProperties.journeyCoordinates, { color: 'blue' }).addTo(appProperties.map);
-    L.marker(latLong).addTo(appProperties.map);
-}
-
-function startServiceWorker() {
-    navigator.serviceWorker.register('./service-worker.js', {
-        scope: './'
-    });
-}
-
 function initSpeedChart() {
     appProperties.speedChart = new Chart(appProperties.speedChartElement, {
         type: 'line',
@@ -163,7 +89,6 @@ function initSpeedChart() {
         }
     });
 }
-
 function updateChartWith(newDataPoint) {
     appProperties.speedHistory.push(newDataPoint);
 
@@ -175,6 +100,78 @@ function updateChartWith(newDataPoint) {
     appProperties.speedChart.update();
 }
 
+// #### Map
+function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                initMap(position.coords)
+            },
+            (error) => console.error("Error getting the current location", error),
+            {enableHighAccuracy: true}
+        );
+    } else {
+        console.warn("Geolocation is not supported by this browser.");
+    }
+}
+
+function initMap(position) {
+    const latLong = [position.latitude,position.longitude];
+    appProperties.map = L.map('map').setView(latLong, 18);
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; Apolo Mc Melo'
+    }).addTo(appProperties.map);
+
+    L.marker(latLong).addTo(appProperties.map);
+}
+
+function updatePositionOnMap(position) {
+    const latLong = [position.latitude,position.longitude];
+
+    appProperties.journeyCoordinates.push(latLong);
+    appProperties.map.setView(latLong, 18);
+
+    L.polyline(appProperties.journeyCoordinates, { color: 'blue' }).addTo(appProperties.map);
+    L.marker(latLong).addTo(appProperties.map);
+}
+
+// #### Business Logic
+function startRecording() {
+    localStorage.clear()
+    appProperties.watchId = navigator.geolocation.watchPosition(
+        (position) => {
+            updatePositionOnMap(position.coords)
+            updateSpeed(position)
+        },
+        (error) => console.error("Error watching the position", error),
+        {enableHighAccuracy: true});
+}
+
+function stopRecording() {
+    navigator.geolocation.clearWatch(appProperties.watchId);
+
+    appProperties.watchId = null;
+    localStorage.setItem("SpeedHistory", JSON.stringify(appProperties.speedHistory))
+
+    metrics.forEach(metric => {
+        const singleMetricHistory = appProperties.metricsHistory.filter(m => m.name === metric.name)
+        localStorage.setItem(`${metric.name}History`, JSON.stringify(singleMetricHistory))
+    })
+
+    console.debug("Speed History", appProperties.speedHistory)
+    console.debug("Metrics History", appProperties.metricsHistory)
+
+    appProperties.speedHistory = []
+    appProperties.metricsHistory = []
+}
+
+// #### Helper Functions
+function formatTimestamp(timestamp) {
+    return new Date(timestamp).toLocaleTimeString('en-US', {hour12: false});
+}
+
 function createChartGradient(chartElement) {
     // Create the gradient for the line
     const gradient = chartElement.createLinearGradient(0, 0, chartElement.canvas.width, 0);
@@ -184,10 +181,54 @@ function createChartGradient(chartElement) {
     return gradient;
 }
 
-function formatTimestamp(timestamp) {
-    return new Date(timestamp).toLocaleTimeString('en-US', {hour12: false});
+// #### UI
+function toggleStartStop() {
+    appProperties.dom.startStopButtonIcon.classList.toggle("ri-play-fill");
+    appProperties.dom.startStopButtonIcon.classList.toggle("ri-stop-fill");
 }
 
+function loadRecordingPage() {
+    appProperties.dom.startStopButtonIcon.classList.add("active");
+    appProperties.dom.settingsPageButton.classList.remove("active");
+    appProperties.dom.reportPageButton.classList.remove("active");
+}
+
+function loadSettingsPage() {
+    appProperties.dom.settingsPageButton.classList.add("active");
+    appProperties.dom.startStopButtonIcon.classList.remove("active");
+    appProperties.dom.reportPageButton.classList.remove("active");
+}
+
+function loadReportPage() {
+    appProperties.dom.reportPageButton.classList.add("active");
+    appProperties.dom.startStopButtonIcon.classList.remove("active");
+    appProperties.dom.settingsPageButton.classList.remove("active");
+}
+
+function registerButtonListeners() {
+    appProperties.dom.startStopButton.addEventListener('click', (event) => {
+        loadRecordingPage();
+
+        if (appProperties.watchId) { // It is stopping
+            stopRecording();
+        } else { // It is starting
+            startRecording();
+
+        }
+        toggleStartStop();
+    });
+    appProperties.dom.settingsPageButton.addEventListener('click', (event) => loadSettingsPage())
+    appProperties.dom.reportPageButton.addEventListener('click', (event) => loadReportPage())
+}
+
+// #### PWA Utils
+function startServiceWorker() {
+    navigator.serviceWorker.register('./service-worker.js', {
+        scope: './'
+    });
+}
+
+// #### Simulation Functions
 function simulateMetricsChange() {
     metrics.forEach((metric) => {
         metric.score = Math.floor(Math.random() * 80) + 20 // Random metric between 20 and 100
@@ -203,6 +244,8 @@ function simulateSpeedChange() {
 }
 
 window.onload = () => {
+    registerButtonListeners()
+
     metrics.forEach(metric => updateMetricsScore(metric))
     getCurrentLocation()
     initSpeedChart()
