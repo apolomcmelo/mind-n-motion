2366541b0c9b9a720b12eeea5d6f28a29e05a09a
Apolo Melo <apolomelo@gmail.com>
2024-12-06 10:19:09 +0000

    Fix the deployment on GH Pages

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -11,15 +11,11 @@ export default {
     [
       '@snowpack/plugin-typescript',
       {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
         ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
       },
     ],
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
+  routes: [],
   packageOptions: {
     polyfillNode: false,
     rollup: {
